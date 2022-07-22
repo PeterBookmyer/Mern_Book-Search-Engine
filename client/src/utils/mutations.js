09a4614fe75dc807ed7b708a48a6095166e880ab
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-07-22 12:35:20 -0600

    fixed spelling mistakes and cleaned up mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -7,7 +7,6 @@ export const LOGIN_USER = gql`
       user {
         _id
         username
-        email
       }
     }
   }
@@ -16,65 +15,44 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-      user {
-          _id
-          username
-          email
-          bookCount
-          savedBooks {
-              authors
-              bookId
-              image
-              link
-              title
-              description
-          }
-      }
       token
+      user {
+        _id
+        username
       }
     }
   }
 `;
+
 export const SAVE_BOOK = gql`
-  mutation saveBook($input: savedBook!) {
+  mutation saveBook($input: SaveBook!) {
     saveBook(input: $input) {
        {
           _id
           username
-          email
-          bookCount
           savedBooks {
-              # _id
-              authors
               bookId
-              image
-              link
               title
+              authors
+              image
               description
+        link
           }
         }
       }
-    }
-  }
 `;
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
-       {
-          _id
-          username
-          email
-          bookCount
-          savedBooks {
-              # _id
-              authors
-              bookId
-              image
-              link
-              title
-              description
-          }
-         }
+      _id
+      username
+      savedBooks {
+        bookId
+        title
+        authors
+        image
+        description
+        link
       }
     }
   }
